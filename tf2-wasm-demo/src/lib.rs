3544ab30c8fed72a37553b9ff771f6a6a4e77ce9
zbkm <zubokome@gmail.com>
2026-02-04 16:10:47 +0500

    feat: add parseing player summary

--- tf2-wasm-demo/src/lib.rs
+++ tf2-wasm-demo/src/lib.rs
@@ -10,13 +10,19 @@ struct JsonDemo {
     header: Header,
     #[serde(flatten)]
     state: MatchState,
+    player_summary: PlayerSummaryState
 }
 
 #[wasm_bindgen]
 pub fn parse(demo_bytes: &[u8]) -> Result<String, JsError> {
     let demo = Demo::new(&demo_bytes);
     let parser = DemoParser::new_all(demo.get_stream());
+    let player_summary = DemoParser::new_with_analyser(demo.get_stream(), PlayerSummaryAnalyzer::new());
     let (header, state) = parser.parse()?;
-    let demo = JsonDemo { header, state };
+    let (_, player_summary_state) = player_summary.parse()?;
+    let demo = JsonDemo { header, state, player_summary: player_summary_state };
     Ok(serde_json::to_string(&demo)?)
 }
+
+use tf_demo_parser::demo::parser::player_summary_analyzer::{PlayerSummaryAnalyzer, PlayerSummaryState};
+
